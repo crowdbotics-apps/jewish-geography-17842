f42a07a1df81c963f0b7ffe2f3612ceef436e87e
Crowdbotics <team@crowdbotics.com>
2020-06-05 15:17:23 +0000

    Update models

--- backend/jewish_geography_17842/wsgi.py
+++ backend/jewish_geography_17842/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jewish_geography_17842.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jewish_geography_17842.settings")
 
 application = get_wsgi_application()
